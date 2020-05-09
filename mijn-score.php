4e37063aa6f71c5015bb25abc2d5cd8dab411883
elinestr <eline.steenredeker@gmail.com>
2020-05-09 20:06:01 +0100

    Update mijn-score.php

---
+++ mijn-score.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "include/head.php"; ?>
+<body>
+<div class="container">
+    <div class="jumbotron homepage mt-3">
+        <div class="row justify-content-between">
+            <div class="col">
+                <i class="fas fa-arrow-left"></i>
+            </div>
+            
+            <div class="col-6">
+                <p class="mijn-score-head-title">Mijn Score</p>
+            </div>
+            
+            <div class="col">
+                <img src="assets/images/icons/winkel.svg" class="jumbo-img-top" alt="...">
+                <img src="assets/images/icons/bell.svg" class="jumbo-img-top" alt="...">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include "include/bottom_navbar.php"; ?>
+<?php include "include/scripts.php"; ?>
+</body>
+</html>
