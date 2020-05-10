23c65e16e7aca4e9c5bcd58d39ebdeb382c1d288
elinestr <eline.steenredeker@gmail.com>
2020-05-10 20:34:02 +0100

    Update mijn-score.php

--- mijn-score.php
+++ mijn-score.php
@@ -4,6 +4,16 @@
 <title>Mijn Score</title>
 
 <body>
+<div class="container">
+    <div class="row justify-content-center">
+   <div class="col routes-buttons">
+  <button type="button" class="btn btn-outline-dark route-page-button active">Mijn Score</button>
+   </div>
+   <div class="col routes-buttons">
+  <button type="button" class="btn btn-outline-dark route-page-button">Scorebord</button>
+   </div>
+ </div>
+</div>
 
 <?php include "include/bottom_navbar.php"; ?>
 <?php include "include/scripts.php"; ?>
