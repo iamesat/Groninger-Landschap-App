108da58bad0feba9af76458567730b2c2f7c3e0b
evaapje <64828027+evaapje@users.noreply.github.com>
2020-05-10 18:38:32 +0200

    groep af

--- groep.php
+++ groep.php
@@ -26,48 +26,98 @@
         <div class="col-12">
             <div class="card groepborder">
                 <div class="card-body">
-                    <h3 class="card-fotorollen-title">Groep bosmannen</h3>
+
+                    <h3 class="card-groep-title">Groep bosmannen</h3>
+
                     <br>
-                    <div class="groepinformatie">
+
+                    <div class="container">
+                    <div class="row">
+                    <div class="col-sm groepinformatie ">
+
                     <p class="groepinfo">Besloten groep</p>
                     <p class="groepinfo">Status: Aanvraag verstuurd</hp>
+                      <p class="meldingengroep"> Geen nieuwe meldingen </p>
                     <br>
-<div class="groepbutton">
-  <p class ="groepaantal"> 2/10 </p>
-  <p class="bekijkenbutton"> bekijken </p>
-</div>
 
-                  <p class="meldingengroep"> Geen nieuwe meldingen </p>
+                  </div>
+
+
+                <div class="col-sm groepbutton ">
+                  <p class ="groepaantal"> 2/10 </p>
+                  <p class="bekijkenbutton"> bekijken </p>
+</div>
                 </div>
-              </div>
+</div>
+</div>
             </div>
         </div>
     </div>
 </div>
 
-<div class="container twee">
+<div class="container groepruimte">
     <div class="row justify-content-md-center">
         <div class="col-12">
-            <div class="card">
+            <div class="card groepborder">
                 <div class="card-body">
-                    <h5 class="card-fotorollen-title">Aantal fotorollen</h5>
+
+                    <h3 class="card-groep-title">Groep Stinkies</h3>
+
                     <br>
-                    <h3 class="fotorollen-nummer">3</h3>
+
+                    <div class="container">
+                    <div class="row">
+                    <div class="col-sm groepinformatie ">
+
+                    <p class="groepinfo">Besloten groep</p>
+                    <p class="groepinfo">Status: Aanvraag verstuurd</hp>
+                      <p class="meldingengroep"> Geen nieuwe meldingen </p>
+                    <br>
+
+                  </div>
+
+
+                <div class="col-sm groepbutton ">
+                  <p class ="groepaantal"> 2/10 </p>
+                  <p class="bekijkenbutton"> bekijken </p>
+</div>
                 </div>
+</div>
+</div>
             </div>
         </div>
     </div>
 </div>
 
-<div class="container twee">
+<div class="container groepruimte">
     <div class="row justify-content-md-center">
         <div class="col-12">
-            <div class="card">
+            <div class="card groepborder">
                 <div class="card-body">
-                    <h5 class="card-fotorollen-title">Aantal fotorollen</h5>
+
+                    <h3 class="card-groep-title">Groep Jacobs College</h3>
+
                     <br>
-                    <h3 class="fotorollen-nummer">3</h3>
+
+                    <div class="container">
+                    <div class="row">
+                    <div class="col-sm groepinformatie ">
+
+                    <p class="groepinfo">Besloten groep</p>
+                    <p class="groepinfo">Status: Aanvraag verstuurd</hp>
+                      <p class="meldingengroep"> Geen nieuwe meldingen </p>
+                    <br>
+
+                  </div>
+
+
+                <div class="col-sm groepbutton ">
+                  <p class ="groepaantal"> 2/10 </p>
+                  <p class="bekijkenbutton"> bekijken </p>
+</div>
                 </div>
+</div>
+</div>
             </div>
         </div>
     </div>
