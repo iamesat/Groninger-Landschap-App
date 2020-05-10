ef7414728e0ecd4605ad3954c9f710c801e00db5
evaapje <64828027+evaapje@users.noreply.github.com>
2020-05-10 09:53:05 +0200

    groep

---
+++ groep.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "include/head.php"; ?>
+<body>
+<div class="container">
+    <div class="jumbotron homepage mt-3">
+        <div class="row justify-content-between">
+            <div class="col">
+                <i class="fas fa-arrow-left"></i>
+            </div>
+
+            <div class="col-6">
+                <p class="mijngroepen-head-title">Mijn Groepen</p>
+            </div>
+
+            <div class="col">
+                <img src="assets/images/icons/winkel.svg" class="jumbo-img-top" alt="...">
+                <img src="assets/images/icons/bell.svg" class="jumbo-img-top" alt="...">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row justify-content-md-center">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-fotorollen-title">Aantal fotorollen</h5>
+                    <br>
+                    <h3 class="fotorollen-nummer">3</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include "include/bottom_navbar.php"; ?>
+<?php include "include/scripts.php"; ?>
+</body>
+</html>
