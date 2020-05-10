4eb909e8513d4ddd874bd546714be1b7d8c9a8a1
niekvondeling <niekvondeling@live.nl>
2020-05-10 23:07:54 +0200

    Update fotorollen pagina
    mapje aangemaakt voor foto's binnenin fotorollen.
    
    poging tot goedmaken van cards en uiterlijk qua ontwerp.
    
    Moeilijkheden met doorverwijslinks onderaan de fotorollen pagina

--- fotorollen.php
+++ fotorollen.php
@@ -4,6 +4,7 @@
 <?php include "include/top_navbar.php"; ?>
 <title>Mijn Routes</title>
 <body>
+<br><br><br><br>
 <div class="container">
     <div class="row justify-content-md-center">
         <div class="col-12">
@@ -11,12 +12,50 @@
                 <div class="card-body">
                     <h5 class="card-fotorollen-title">Aantal fotorollen</h5>
                     <br>
-                    <h3 class="fotorollen-nummer">3</h3>
+                    <h2 class="fotorollen-nummer">3</h2>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body text-center">
+                    <p>Mijn recente foto's</p>
+                    <div class="row justify-content-center">
+                    <a href="" class="stretched-link" data-transition="slide" rel="external"><img src="assets/images/fotorollen/molen.png" class="card-img-top" alt="..."></a>
+                    <a href="" class="stretched-link" data-transition="slide" rel="external"><img src="assets/images/fotorollen/huis.png" class="card-img-top" alt="..."></a>
+                    </div>
+                    <div class="row justify-content-center">
+                    <a href="" class="stretched-link" data-transition="slide" rel="external"><img src="assets/images/fotorollen/beekje.png" class="card-img-top" alt="..."></a>
+                    <a href="" class="stretched-link" data-transition="slide" rel="external"><img src="assets/images/fotorollen/huis2.png" class="card-img-top" alt="..."></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body text-center">
+                    <p>Mijn foto overzicht</p>
+                    <p>Groepen foto overzicht</p>
+                    <p>Fotowedstrijd</p>
+                    <p>Nieuwe fotorollen aanschaffen</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <?php include "include/bottom_navbar.php"; ?>
 <?php include "include/scripts.php"; ?>
 </body>
