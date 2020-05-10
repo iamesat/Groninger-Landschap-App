0a9ce09aecfa2a609f5c9089a8feca274bf54d2a
elinestr <eline.steenredeker@gmail.com>
2020-05-10 20:53:03 +0100

    Update mijn-score.php

--- mijn-score.php
+++ mijn-score.php
@@ -16,23 +16,29 @@
 </div>
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-            <div class="card-body">
-                <p class="card-text">Level</p>
-                <h3 class="card-title">2</h3>
+    <div class="row justify-content-center mx-4">
+    <div class="col-sm">
+            <div class="card text-white bg-primary mb-3 mx-2" style="max-width: 18rem;">
+                <div class="card-body text-center">
+                    <p class="card-text">Level</p>
+                    <h3 class="card-title">2</h3>
+                </div>
             </div>
         </div>
-        <div class="card text-white bg-warning mb-3">
-            <div class="card-body">
-                <p class="card-text">Plaats</p>
-                <h3 class="card-title">1e</h3>
+        <div class="col-sm">
+            <div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
+                <div class="card-body text-center">
+                    <p class="card-text">Plaats</p>
+                    <h3 class="card-title">1e</h3>
+                </div>
             </div>
         </div>
-        <div class="card text-white bg-danger mb-3">
-            <div class="card-body">
-                <p class="card-text">Aantal quizen goed</p>
-                <h3 class="card-title">14</h3>
+        <div class="col-sm">
+            <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+                <div class="card-body text-center">
+                    <p class="card-text">Aantal quizen goed</p>
+                    <h3 class="card-title">14</h3>
+                </div>
             </div>
         </div>
 
