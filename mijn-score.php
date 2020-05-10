d1b2ddaa711a1d6e0493871ae1eec5e0a6d031a3
elinestr <eline.steenredeker@gmail.com>
2020-05-10 20:44:28 +0100

    Update mijn-score.php

--- mijn-score.php
+++ mijn-score.php
@@ -4,7 +4,7 @@
 <title>Mijn Score</title>
 
 <body>
-<div class="container mx-auto">
+<div class="container">
     <div class="row justify-content-center">
    <div class="col routes-buttons">
   <button type="button" class="btn btn-outline-dark route-page-button active">Mijn Score</button>
@@ -13,6 +13,30 @@
   <button type="button" class="btn btn-outline-dark route-page-button">Scorebord</button>
    </div>
  </div>
+</div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+            <div class="card-body">
+                <p class="card-text">Level</p>
+                <h3 class="card-title">2</h3>
+            </div>
+        </div>
+        <div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
+            <div class="card-body">
+                <p class="card-text">Plaats</p>
+                <h3 class="card-title">1e</h3>
+            </div>
+        </div>
+        <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+            <div class="card-body">
+                <p class="card-text">Aantal quizen goed</p>
+                <h3 class="card-title">14</h3>
+            </div>
+        </div>
+
+    </div>
 </div>
 
 <?php include "include/bottom_navbar.php"; ?>
