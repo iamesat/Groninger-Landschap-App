d63dedaf6c8bf6b56b8bd01865c6def296f57159
elinestr <eline.steenredeker@gmail.com>
2020-05-13 13:57:26 +0100

    shop

--- shop.php
+++ shop.php
@@ -4,7 +4,24 @@
 <title>Shop</title>
 <body>
 <?php include "include/top_navbar.php"; ?>
-
+<br><br><br><br>
+<div class="container mt-4">
+    <div class="row justify-content-md-center">
+        <div class="col-12">
+            <div class="card scoreborder">
+                <div class="card-body my-3">
+                    <div class="row">
+                        <div class="col">
+                            <h3 class="text-center" style="font-size: 14px;">Aantal fotorollen</h3>
+                            <p class="text-center" style="font-size: 50px;">20</p>
+                            <button type="button" class="btn btn-outline-light shop-button">Nieuwe aanschaffen</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <?php include "include/bottom_navbar.php"; ?>
