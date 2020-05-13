aa573f836251999b9bfd099c269542c8918dbb86
niekvondeling <niekvondeling@live.nl>
2020-05-13 18:10:19 +0200

    changes foto overzicht
    Buttons toegevoegd in de style net als scorepage. Leek me mooier om in de zelfde pagina met een button te switchen tussen eigen foto's en groepsfoto's.

--- foto-overzicht.php
+++ foto-overzicht.php
@@ -8,11 +8,11 @@
 <div class="container text-center my-3">
     <div class="row justify-content-center">
         <div class="col fotopage-buttons">
-            <a href="foto-overzicht.php" class="stretched-link" data-transition="slide" rel="external"><button type="button" class="btn btn-outline-dark foto-page-button">Mijn foto's</button>
+            <a href="foto-overzicht.php" class="stretched-link" data-transition="slide" rel="external"><button type="button" class="btn btn-outline-dark foto-page-button active">Mijn foto's</button>
         </div>
-
+   
         <div class="col foto-buttons">
-            <a href="groepen-foto-overzicht.php" class="stretched-link" data-transition="slide" rel="external"><button type="button" class="btn btn-outline-dark foto-page-button active">Mijn groepen foto's</button></a>
+            <a href="groepen-foto-overzicht.php" class="stretched-link" data-transition="slide" rel="external"><button type="button" class="btn btn-outline-dark foto-page-button">Mijn groepen foto's</button></a>
         </div>
     </div>
 </div>
