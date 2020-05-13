bf3fd8ab0d8757254c21082b2d1b379906082389
elinestr <eline.steenredeker@gmail.com>
2020-05-13 13:17:53 +0100

    shop.php

---
+++ shop.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "include/head.php"; ?>
+<title>Fotorollen</title>
+<body>
+<?php include "include/top_navbar.php"; ?>
+
+
+
+<?php include "include/bottom_navbar.php"; ?>
+<?php include "include/scripts.php"; ?>
+</body>
+</html>
