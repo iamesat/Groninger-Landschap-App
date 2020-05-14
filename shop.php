f87e3f430e6d4c7a69bfdc88cd5f79a4aac4ec56
elinestr <eline.steenredeker@gmail.com>
2020-05-14 16:41:18 +0100

    css en shop veranderd

--- shop.php
+++ shop.php
@@ -29,12 +29,12 @@
     </div>
 </div>
 
-<div class="container mt-4">
+<div class="container my-4">
     <div class="row">
         <div class="col">
             <h3 class="text-center pt-3" style="font-size: 14px">Doneer het Groninger landschap</h3>
             <div class="input-group input-group-sm mb-3">
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                <input type="text" class="form-control" style="width: 116px;" placeholder="€" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
             </div>
             <button type="button" class="btn btn-outline-light shop-button">Doneer</button>
         </div>
