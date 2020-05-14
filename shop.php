146549b48c0b4c9f36a9e99cbf14c799461ab528
elinestr <eline.steenredeker@gmail.com>
2020-05-14 16:49:02 +0100

    shop pagina verandert

--- shop.php
+++ shop.php
@@ -5,12 +5,14 @@
 <body>
 <?php include "include/top_navbar.php"; ?>
 <br><br><br><br>
+
+
 <div class="container mt-4">
-    <div class="row justify-content-md-center">
-        <div class="col-12">
+    <div class="row text-center">
+        <div class="col">
             <div class="card scoreborder">
                 <div class="card-body my-3">
-                    <h3 class="text-center" style="font-size: 14px;">Aantal fotorollen</h3>
+                    <h3 class="text-center" style="font-size: 16px;">Aantal fotorollen</h3>
                     <p class="text-center" style="font-size: 50px;">20</p>
                     <button type="button" class="btn btn-outline-light shop-button">Nieuwe aanschaffen</button>
                 </div>
@@ -20,26 +22,36 @@
 </div>
 
 <div class="container mt-4">
-    <div class="row">
+    <div class="row text-center">
         <div class="col">
-            <h3 class="text-center pt-3" style="font-size: 14px">Upgrade naar PRO</h3>
-            <p class="text-center pt-2" style="font-size: 30px;">€2,99<br>per maand</p>
-            <button type="button" class="btn btn-outline-light shop-button">Upgrade</button>
+            <div class="card scoreborder">
+                <div class="card-body my-3">
+                    <h3 class="text-center" style="font-size: 16px">Upgrade naar PRO</h3>
+                    <p class="text-center pt-2" style="font-size: 30px;">€2,99<br>per maand</p>
+                    <button type="button" class="btn btn-outline-light shop-button">Upgrade</button>
+                </div>
+            </div>
         </div>
     </div>
 </div>
 
 <div class="container my-4">
-    <div class="row">
+    <div class="row text-center">
         <div class="col">
-            <h3 class="text-center pt-3" style="font-size: 14px">Doneer het Groninger landschap</h3>
-            <div class="input-group input-group-sm mb-3">
-                <input type="text" class="form-control" style="width: 116px;" placeholder="€" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+        <div class="card scoreborder">
+                <div class="card-body my-3">
+                    <h3 style="font-size: 16px">Doneer het Groninger landschap</h3>
+                    <div class="input-group input-group-sm my-4">
+                        <input type="text" class="form-control" style="width: 116px;" placeholder="€" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                    </div>
+                    <button type="button" class="btn btn-outline-light shop-button">Doneer</button>
+                </div>
             </div>
-            <button type="button" class="btn btn-outline-light shop-button">Doneer</button>
         </div>
     </div>
 </div>
+
+<br><br><br><br>
 
 
 
