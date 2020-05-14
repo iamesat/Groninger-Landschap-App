a8fe64862ae77e97a629acdf6b8a114ef9c4731a
elinestr <eline.steenredeker@gmail.com>
2020-05-14 14:58:19 +0100

    Update shop.php

--- shop.php
+++ shop.php
@@ -10,18 +10,35 @@
         <div class="col-12">
             <div class="card scoreborder">
                 <div class="card-body my-3">
-                    <div class="row">
-                        <div class="col">
-                            <h3 class="text-center" style="font-size: 14px;">Aantal fotorollen</h3>
-                            <p class="text-center" style="font-size: 50px;">20</p>
-                            <button type="button" class="btn btn-outline-light shop-button">Nieuwe aanschaffen</button>
-                        </div>
-                    </div>
+                    <h3 class="text-center" style="font-size: 14px;">Aantal fotorollen</h3>
+                    <p class="text-center" style="font-size: 50px;">20</p>
+                    <button type="button" class="btn btn-outline-light shop-button">Nieuwe aanschaffen</button>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<div class="container mt-4">
+    <div class="row">
+        <div class="col">
+            <h3 class="text-center pt-3" style="font-size: 14px">Upgrade naar PRO</h3>
+            <p class="text-center pt-2" style="font-size: 30px;">€2,99<br>per maand</p>
+            <button type="button" class="btn btn-outline-light shop-button">Upgrade</button>
+        </div>
+    </div>
+</div>
+
+<div class="container mt-4">
+    <div class="row">
+        <div class="col">
+            <h3 class="text-center pt-3" style="font-size: 14px">Doneer het Groninger landschap</h3>
+            
+            <button type="button" class="btn btn-outline-light shop-button">Doneer</button>
+        </div>
+    </div>
+</div>
+
 
 
 <?php include "include/bottom_navbar.php"; ?>
