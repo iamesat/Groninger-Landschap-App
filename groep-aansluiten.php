d6bfdde0ed889121cc30f570062f5d279af0ac86
evaapje <64828027+evaapje@users.noreply.github.com>
2020-05-16 14:05:33 +0200

    aansluiten

---
+++ groep-aansluiten.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "include/head.php"; ?>
+<?php include "include/top_navbar.php"; ?>
+<title>Aansluiten bij een groep </title>
+<body>
+
+  <div class="container groepruimte groepboven">
+      <div class="row justify-content-md-center">
+          <div class="col-12">
+              <div class="card groepborder">
+                  <div class="card-body">
+
+                      <h3 class="card-groep-title alleenspelen">Speel alleen of maak een groep aan</h3>
+
+
+    <p class="groepinfo2">Wil je liever alleen spelen of wil je een groep aanmaken?</p>
+    <br>
+                      <div class="container">
+                      <div class="row">
+                      <div class="col-sm groepinformatie ">
+
+
+                        <p class="bekijkenbutton"> bekijken </p>
+
+                      <br>
+
+                    </div>
+
+
+                  <div class="col-sm  groepbutton2 ">
+
+                    <p class="bekijkenbutton"> bekijken </p>
+  </div>
+                  </div>
+  </div>
+  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+
+<div class="container groepruimte ">
+    <div class="row justify-content-md-center">
+        <div class="col-12">
+            <div class="card groepborder">
+                <div class="card-body">
+
+                    <h3 class="card-groep-title">Groep bosmannen</h3>
+
+                    <br>
+
+                    <div class="container">
+                    <div class="row">
+                    <div class="col-sm groepinformatie ">
+
+                    <p class="groepinfo">Besloten groep</p>
+                    <p class="groepinfo">Alleen mensen uit mijn vriendengroep mogen aansluiten</p>
+
+                    <br>
+
+                  </div>
+
+
+                <div class="col-sm groepbutton ">
+                  <p class ="groepaantal"> 2/10 </p>
+                  <a href="" class="stretched-link" data-transition="slide" rel="external" data-toggle="modal" data-target="#UpgradeModal"><p class="bekijkenbutton"> Aansluiten </p></a>
+</div>
+                </div>
+</div>
+</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container groepruimte">
+    <div class="row justify-content-md-center">
+        <div class="col-12">
+            <div class="card groepborder">
+                <div class="card-body">
+
+                    <h3 class="card-groep-title">Groep Stinkies</h3>
+
+                    <br>
+
+                    <div class="container">
+                    <div class="row">
+                    <div class="col-sm groepinformatie ">
+
+                    <p class="groepinfo">Besloten groep</p>
+                    <p class="groepinfo">Iedereen mag zich aansluiten</p>
+
+                    <br>
+
+                  </div>
+
+
+                <div class="col-sm groepbutton ">
+                  <p class ="groepaantal"> 2/10 </p>
+                    <a href="" class="stretched-link" data-transition="slide" rel="external" data-toggle="modal" data-target="#UpgradeModal"><p class="bekijkenbutton"> Aansluiten </p></a>
+</div>
+                </div>
+</div>
+</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container groepruimte">
+    <div class="row justify-content-md-center">
+        <div class="col-12">
+            <div class="card groepborder">
+                <div class="card-body">
+
+                    <h3 class="card-groep-title">Groep Jacobs College</h3>
+
+                    <br>
+
+                    <div class="container">
+                    <div class="row">
+                    <div class="col-sm groepinformatie ">
+
+                    <p class="groepinfo">Openbare groep</p>
+                    <p class="groepinfo">Alleen aansluiten als je van de Jacobs College komt</p>
+
+                    <br>
+
+                  </div>
+
+
+                <div class="col-sm groepbutton ">
+                  <p class ="groepaantal"> 2/10 </p>
+                    <a href="" class="stretched-link" data-transition="slide" rel="external" data-toggle="modal" data-target="#UpgradeModal"><p class="bekijkenbutton"> Aansluiten </p></a>
+</div>
+                </div>
+</div>
+</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div class="modal fade" id="UpgradeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header modal-shop-header">
+                <h5 class="modal-title" id="exampleModalLabel">Upgrade naar Pro informatie</h5>
+                <button type="button" class="close close-shop" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>
+                    <b>Informatie over pro:</b><br>
+                    <br>
+                    - Advertentievrije applicatie<br>
+                    - Elke maand 3 gratis fotorollen<br>
+                    - Je mag 2 foto's insturen bij een wedstrijd.<br>
+                    - Je helpt het goede doel!
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary btn-info-shop" data-dismiss="modal">Bevestigen</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php include "include/bottom_navbar.php"; ?>
+<?php include "include/scripts.php"; ?>
+</body>
+</html>
