1f3cde3f69d393211a13488c9e5d4ec6eaef2448
Annet <aeyavas@outlook.com>
2020-05-16 14:51:33 +0200

    proper.js toegevoegd voor dropdown etc
    proper.js toegevoegd voor dropdown etc

--- include/scripts.php
+++ include/scripts.php
@@ -1,7 +1,8 @@
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+<link href="assets/js/bootstrap.bundle.js" rel="stylesheet">
 <script src="assets/js/bootstrap.min.js"></script>
-
 
 <script> document.getElementById("paginatitel").innerHTML = document.title;
 
