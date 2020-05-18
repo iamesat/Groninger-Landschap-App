b0a6af0c888a76f83ccd6f380d1231c0b599e9a9
Annet <aeyavas@outlook.com>
2020-05-18 11:01:59 +0200

    bottom navbar gefixt
    bottom navbar gefixt

--- include/bottom_navbar.php
+++ include/bottom_navbar.php
@@ -3,23 +3,28 @@
         <div class="container">
             <div class="row row-cols-5">
               <div class="col">
+                <a class="linkje" href="home.php">
                 <img src="assets/images/icons/home.svg" class="navbar-bottom-icons" alt="home" />
-
                 Home
+              </a>
               </div>
               <div class="col">
                 &nbsp;
               </div>
               <div class="col">
+                <a class="linkje" href="#">
                 <img src="assets/images/icons/gps.svg" class="navbar-bottom-icons" alt="home" />
                 Route
+              </a>
               </div>
               <div class="col">
                 &nbsp;
               </div>
               <div class="col">
+                <a class="linkje" href="#">
                 <img src="assets/images/icons/chatten.svg" class="navbar-bottom-icons" alt="home" />
                 Chats
+              </a>
               </div>
       </div>
       </div>
