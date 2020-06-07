7854bdd752e3898277988650a2282e801774fd81
niekvondeling <niekvondeling@live.nl>
2020-06-07 22:01:30 +0200

    Update top_navbar.php
    Meldingen en links toevoegen voor verbinding met fotopagina / QR scannen / Groep aanmaken

--- include/top_navbar.php
+++ include/top_navbar.php
@@ -19,22 +19,21 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header modal-shop-header">
-                <h5 class="modal-title" id="exampleModalLabel">Deze foto uploaden?</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Meldingen</h5>
                 <button type="button" class="close close-shop" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <p>
-                    Locatie: wandelroute Iwema Steenhuis<br><br>
-                    Wil je deze foto inzenden voor de fotowedstrijd? <br><br>
-                    Klik dan op 'JA'. Wil je er nog over nadenken? Klik dan op 'NEE'.
+                   <a href="fotowedstrijd" data-transition="slide" rel="external">Je loopt nu in de buurt van het Iwema steenhuis, dat is een fotowedstrijd!</a><br><br>
+                   <a href="qr-scanner" data-transition="slide" rel="external">Kijk goed op je kaart, kijk goed om je heen! Een QR-code bevindt zich in jouw buurt! </a> <br><br>
+                   <a href="foto-overzicht" data-transition="slide" rel="external">Nog geen foto ingezonden voor de foto wedstrijd? Doe het snel en verzamel zoveel mogelijk stemmen! </a>
+                   <a href="groep" data-transition="slide" rel="external">Je bent uitgenodigd voor de groep: Ontbijtgranen en melk. Ga snel naar groepen om de uitnodiging te bekijken! </a>  
                 </p>
-                <img src="assets/images/fotorollen/wedstrijd.svg">
             </div>
             <div class="modal-footer">
-            <a href="foto-upload" class="stretched-link" data-transition="slide" rel="external"><button type="button" class="btn btn-secondary btn-info-shop" data-dismiss="modal">JA</button></a>
-            <button type="button" class="btn btn-secondary btn-info-shop" data-dismiss="modal">NEE</button>
+            <a href="" data-transition="slide" rel="external"><button type="button" class="btn btn-secondary btn-info-shop" data-dismiss="modal">Sluit meldingen</button></a>
             </div>
         </div>
     </div>
