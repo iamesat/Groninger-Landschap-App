fe702db8c04ece44b927f2c2de79c5f327e884b6
niekvondeling <niekvondeling@live.nl>
2020-06-07 21:16:09 +0200

    Update top_navbar.php
    Proberen modal toe te voegen aan medlingen maar werkt niet?

--- include/top_navbar.php
+++ include/top_navbar.php
@@ -9,9 +9,35 @@
             </div>
             <div class="col-auto">
               <a href="shop.php" class="stretched-link" data-transition="slide" rel="external"><img src="assets/images/icons/winkel.svg" class="jumbo-img-top" alt="..."></a>
-              <img src="assets/images/icons/bell.svg" class="jumbo-img-top" alt="...">
+              <a href="" data-transition="slide" rel="external" data-toggle="modal" data-target="#UpgradeModal"><img src="assets/images/icons/bell.svg" class="jumbo-img-top" alt="..."></a>
             </div>
           </div>
         </div>
     </nav>
+
+    <div class="modal fade" id="UpgradeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header modal-shop-header">
+                <h5 class="modal-title" id="exampleModalLabel">Deze foto uploaden?</h5>
+                <button type="button" class="close close-shop" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>
+                    Locatie: wandelroute Iwema Steenhuis<br><br>
+                    Wil je deze foto inzenden voor de fotowedstrijd? <br><br>
+                    Klik dan op 'JA'. Wil je er nog over nadenken? Klik dan op 'NEE'.
+                </p>
+                <img src="assets/images/fotorollen/wedstrijd.svg">
+            </div>
+            <div class="modal-footer">
+            <a href="foto-upload" class="stretched-link" data-transition="slide" rel="external"><button type="button" class="btn btn-secondary btn-info-shop" data-dismiss="modal">JA</button></a>
+            <button type="button" class="btn btn-secondary btn-info-shop" data-dismiss="modal">NEE</button>
+            </div>
+        </div>
+    </div>
+</div>
+
   <div class="app-breaks">
