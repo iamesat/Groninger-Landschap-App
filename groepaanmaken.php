efcfc4672bde0302238eb8106603889724547ffe
evaap <61408757+evaap@users.noreply.github.com>
2020-06-09 18:03:58 +0200

    groewp

--- groepaanmaken.php
+++ groepaanmaken.php
@@ -152,17 +152,15 @@
                     <div class="container">
 
                       <div class="input-group">
-  <div class="input-group-prepend">
-    <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+    <div class="input-group-prepend">
+      <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+    </div>
+    <div class="custom-file">
+      <input type="file" class="custom-file-input" id="inputGroupFile01"
+        aria-describedby="inputGroupFileAddon01">
+      <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+    </div>
   </div>
-  <div class="custom-file">
-    <input type="file" class="custom-file-input" id="inputGroupFile01"
-      aria-describedby="inputGroupFileAddon01">
-    <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
-  </div>
-</div>
-  </div>
-</div>
 
                   </div>
 </div>
--- groepbeheerder.php
+++ groepbeheerder.php
@@ -71,7 +71,7 @@ Beheerder
 
 
   <br>
-  <div class="progress">
+  <div class="progress progressbeheerder">
     <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
   </div>
   <br>
