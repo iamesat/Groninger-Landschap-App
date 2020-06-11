64aab653c167c64c62b70f5edf6a7102cb6443fa
niekvondeling <niekvondeling@live.nl>
2020-06-11 14:26:30 +0200

    Update route.php
    Fix route.php overwrite

--- route.php
+++ route.php
@@ -35,10 +35,10 @@
               <div class="jumbotron route mt-3">
                 <div class="row justify-content-between">
                   <div class="col-auto mr-auto">
-                    <i class="fa fa-bell jumbo-icons"></i>
+                  <a href="" data-transition="slide" rel="external" data-toggle="modal" data-target="#MeldingenModal"><img src="assets/images/icons/bell.svg" class="jumbo-img-top" alt="..."></a>
                   </div>
                   <div class="col-auto">
-                    <i class="fa fa-shopping-cart jumbo-icons"></i>
+                  <a href="shop.php" class="stretched-link" data-transition="slide" rel="external"><i class="fa fa-shopping-cart jumbo-icons"></i></a>
                   </div>
                 </div>
 
@@ -585,7 +585,35 @@
             </div>
             </div>
           </nav>
+          
+    <div class="modal fade" id="MeldingenModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+          <div class="modal-content">
+              <div class="modal-header modal-shop-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Meldingen</h5>
+                  <button type="button" class="close close-shop" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                  </button>
+              </div>
+
+            <div class="modal-body">
+                <p>
+                   <a href="fotowedstrijd" class="melding" data-transition="slide" rel="external">Je loopt nu in de buurt van het Iwema steenhuis, dat is een <a href="fotowedstrijd">fotowedstrijd!</a></a><br><br>
+                   <a href="qr-scanner" class="melding" data-transition="slide" rel="external">Kijk goed op je kaart, kijk goed om je heen! Een <a href="qr-scanner">QR-code</a><a href="qr-scanner" class="melding" data-transition="slide" rel="external"> bevindt zich in jouw buurt!</a></a> <br><br>
+                   <a href="foto-overzicht" class="melding" data-transition="slide" rel="external">Nog geen foto ingezonden voor de foto wedstrijd? <a href="foto-overzicht">Doe het snel</a><a href="foto-overzicht" class="melding" data-transition="slide" rel="external"> en verzamel zoveel mogelijk stemmen!</a></a> <br><br>
+                   <a href="groep" class="melding" data-transition="slide" rel="external">Je bent uitgenodigd voor de groep: Ontbijtgranen en melk. Ga snel naar <a href="groep">groepen</a><a href="groep" class="melding" data-transition="slide" rel="external"> om de uitnodiging te bekijken!</a></a>
+                </p>
+            </div>
+
+            <div class="modal-footer">
+            <a href="" data-transition="slide" rel="external"><button type="button" class="btn btn-secondary btn-info-shop" data-dismiss="modal">Sluit meldingen</button></a>
+            </div>
+          </div>
+      </div>
+    </div>
+
           <?php include "include/bottom_navbar.php"; ?>
+          <?php include "include/scripts.php"; ?>
     </body>
     </html>
 
