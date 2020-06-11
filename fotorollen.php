8e675721c93aacf4a3fc5e1216f9c4d8980c2536
niekvondeling <niekvondeling@live.nl>
2020-06-11 15:23:24 +0200

    Info modal toegevoegd fotorollen
    Voordat je fotorollen bekijke verschijnt er een modal met informatie.

--- fotorollen.php
+++ fotorollen.php
@@ -51,6 +51,38 @@
     </div>
 </div>
 
+<div class="modal fade" id="Fotorollen-Modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+          <div class="modal-content">
+              <div class="modal-header modal-shop-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Over Fotorollen</h5>
+                  <button type="button" class="close close-shop" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                  </button>
+              </div>
+
+            <div class="modal-body">
+                <p>
+                Welkom bij je fotorollen pagina! Op deze pagina kan je zien hoeveel fotorollen je hebt. Met een fotorol kan je 10 foto's maken tijdens de routes die je volgt. 
+                Bij mijn overzicht zie je al jouw gemaakte foto's, inclusief de foto's waarmee jij prijzen hebt verdiend. 
+                Je kan ook de foto's van al je groepsleden bekijken.
+                De prijzen zijn te verdienen met de fotowedstrijd! Hierin zul je een fotolocatie zien op een bepaalde route. 
+                Het is aan jou om tijdens je route deze foto het beste na te maken en deze vervolgens in te zenden. 
+                Daarna moet je de meeste stemmen verzamelen. Beland je in de top 3? Dan val je in de prijzen. 
+                De prijzen verschillen per wedstrijd. Dit kunnen punten zijn om nieuwe routes te unlocken, maar ook nieuwe fotorollen! 
+                Ook krijg je een medaille voor de foto's waarmee je wint!
+                Ook kan je hier je stem uitbrengen op andere foto's. Voor elke foto die je inzend, moet je ook op een stemmen. 
+                Zo blijft het eerlijk voor iedereen!
+                </p>
+            </div>
+
+            <div class="modal-footer">
+            <a href="" data-transition="slide" rel="external"><button type="button" class="btn btn-secondary btn-info-shop" data-dismiss="modal">Begrepen</button></a>
+            </div>
+          </div>
+      </div>
+    </div>
+
 <?php include "include/bottom_navbar.php"; ?>
 <?php include "include/scripts.php"; ?>
 </body>
